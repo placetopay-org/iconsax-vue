512b34aeaf4331043f3f9a0a5bb8255d95a694e0
JuanZea <vikzea@gmail.com>
2024-02-21 21:09:39 -0500

    feat: responsive mode on web

--- web/src/components/ControlBar.vue
+++ web/src/components/ControlBar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { STab, STabItem, SInput } from '@placetopay/spartan-vue'
-import { FilterSearchIcon } from '@placetopay/iconsax-vue/twotone';
+import { STab, STabItem, SInput, SDropdown, SDropdownItem } from '@placetopay/spartan-vue'
+import { FilterSearchIcon, LayerIcon } from '@placetopay/iconsax-vue/twotone';
 import { $activeStyle, $query, $activeColor } from '@/store';
 import { useStore } from '@nanostores/vue';
 
@@ -18,21 +18,24 @@ const style = useStore($activeStyle)
 </script>
 
 <template>
-    <div class="flex justify-center flex-col items-center gap-4 w-fit">
-        <SInput :right-icon="FilterSearchIcon" :modelValue="query" @update:model-value="$query.set" placeholder="Search all icons" />
-        <section class="w-full flex justify-between items-center gap-4">
-            <STab :modelValue="style" @update:model-value="$activeStyle.set" variant="vetches">
+    <div class="flex justify-center flex-col items-center gap-4">
+        <section class="flex gap-4">
+            <SInput class="self-start" :right-icon="FilterSearchIcon" :modelValue="query" @update:model-value="$query.set" placeholder="Search all icons" />
+            
+            <div class="flex h-[42px] border relative border-gray-300 bg-gray-100 p-1 rounded shadow">
+                <input type="color" class="h-full" :model-value="color" @input="changeColorDebounce" />
+            </div>
+        </section>
+
+        <section class="overflow-x-auto w-full">
+            <STab class="mx-auto w-fit" :modelValue="style" @update:model-value="$activeStyle.set" variant="vetches">
                 <STabItem>Bold</STabItem>
                 <STabItem>Outline</STabItem>
                 <STabItem>Bulk</STabItem>
                 <STabItem>Linear</STabItem>
                 <STabItem>TwoTone</STabItem>
                 <STabItem>Broken</STabItem>
-            </STab>
-
-            <div class="flex h-[53px] border relative border-gray-300 bg-gray-100 p-2 rounded-xl shadow">
-                <input type="color" class="h-full" :model-value="color" @input="changeColorDebounce" />
-            </div>
+            </STab>  
         </section>
     </div>
 </template>
