9a49bc8be817c68960279bf3e34c5c6daca14a48
JuanZea <vikzea@gmail.com>
2024-02-27 12:12:31 -0500

    feat: recognition to iconsax

--- web/src/components/Copyright.vue
+++ web/src/components/Copyright.vue
@@ -5,6 +5,6 @@ import { CopyrightIcon } from '@placetopay/iconsax-vue/bold'
 <template>
     <div class="relative text-sm bg-emerald-50 border border-emerald-100 shadow-sm rounded px-2 py-1 flex">
         <CopyrightIcon class="absolute w-5 h-5 text-[#42b883] -right-2 -top-2" />
-        <p class="text-center">All icons provide by <a class="text-[#42b883]" target="_blank" href="https://iconsax.io/">Iconsax</a></p>
+        <p class="text-center">All icons are provided by <a class="text-[#42b883]" target="_blank" href="https://iconsax.io/">Iconsax</a></p>
     </div>
 </template>
