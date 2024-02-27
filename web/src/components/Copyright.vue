41caca258bf1e02308cde76879c78f2e391c6adb
JuanZea <vikzea@gmail.com>
2024-02-27 12:10:59 -0500

    feat: recognition to iconsax

---
+++ web/src/components/Copyright.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { CopyrightIcon } from '@placetopay/iconsax-vue/bold'
+</script>
+
+<template>
+    <div class="relative text-sm bg-emerald-50 border border-emerald-100 shadow-sm rounded px-2 py-1 flex">
+        <CopyrightIcon class="absolute w-5 h-5 text-[#42b883] -right-2 -top-2" />
+        <p class="text-center">All icons provide by <a class="text-[#42b883]" target="_blank" href="https://iconsax.io/">Iconsax</a></p>
+    </div>
+</template>
